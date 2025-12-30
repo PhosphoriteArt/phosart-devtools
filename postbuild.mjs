8eaba96ca8cea31f3a8b6e25683456f956c50265
Phosphorite <dew@phosphorite.art>
2025-12-30 02:14:00 -0800

    sharp and open

--- postbuild.mjs
+++ postbuild.mjs
@@ -7,9 +7,26 @@ import path from 'node:path';
 const rt = path.dirname(fileURLToPath(import.meta.url));
 const indexPath = path.join(rt, 'build', 'index.js');
 
-const index = readFileSync(indexPath, { encoding: 'utf-8' }).replace("env('HOST', '0.0.0.0')", "env('HOST', '127.0.0.1')");
+const index = readFileSync(indexPath, { encoding: 'utf-8' }).replace(
+	"env('HOST', '0.0.0.0')",
+	"env('HOST', '127.0.0.1')"
+);
 
-writeFileSync(indexPath, '#!/usr/bin/env node\n' + index, { encoding: 'utf-8' });
-chmodSync(indexPath, 0o755)
+writeFileSync(
+	indexPath,
+	`#!/usr/bin/env node
 
-console.log("Successfully updated build/index.js")
+import opener from "opener";
+
+setTimeout(() => {
+  const p = typeof port === "undefined" ? 3000 : port;
+  opener("http://127.0.0.1:" + port);
+}, 200);
+
+${index}
+`,
+	{ encoding: 'utf-8' }
+);
+chmodSync(indexPath, 0o755);
+
+console.log('Successfully updated build/index.js');
--- vite.config.ts
+++ vite.config.ts
@@ -38,5 +38,10 @@ export default defineConfig({
 				}
 			}
 		]
+	},
+	build: {
+		rollupOptions: {
+			external: ['sharp']
+		}
 	}
 });
