6cfb430afbc9397ca49e8ba72e10ab3a5355f147
Phosphorite <dew@phosphorite.art>
2026-01-01 01:33:24 -0800

    add preview

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -8,6 +8,18 @@ export const load: LayoutServerLoad = async () => {
 	const galleryPaths = Object.keys(await rawGalleries());
 	const onlyPath = galleryPaths.length === 1 ? galleryPaths[0] : null;
 	return {
-		redirectGallery: normalizeGalleryPath(onlyPath) || null
+		redirectGallery: normalizeGalleryPath(onlyPath) || null,
+		previewPort: getPreviewPort()
 	};
 };
+
+function getPreviewPort() {
+	try {
+		if (!process.env.PREVIEWPORT) {
+			return null;
+		}
+		return parseInt(process.env.PREVIEWPORT);
+	} catch {
+		return null;
+	}
+}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,16 +6,65 @@
 	import NavLink from '$lib/nav/NavLink.svelte';
 	import { createSharedEpoch } from '$lib/epoch.svelte';
 	import { createSharedGalleryOverrides } from '$lib/galleryoverride.svelte';
+	import { onMount } from 'svelte';
 
 	let { children, data } = $props();
 
+	let showPreview = $state(false);
+
 	createSharedEpoch();
 	createSharedGalleryOverrides();
+
+	onMount(() => {
+		if (!data.previewPort) return;
+
+		const f = (k: KeyboardEvent) => {
+			if (k.code === 'Escape') {
+				showPreview = false;
+			}
+		};
+
+		window.addEventListener('keypress', f, { capture: true });
+		return () => {
+			window.removeEventListener('keypress', f, { capture: true });
+		};
+	});
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
+
+{#if showPreview && data.previewPort}
+	<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<div
+		tabindex="-1"
+		role="figure"
+		class="fixed top-0 right-0 bottom-0 left-0 bg-[#777A]"
+		onclick={(e) => {
+			if (e.target === e.currentTarget) {
+				showPreview = false;
+			}
+		}}
+	>
+		<div class="fixed top-20 right-20 bottom-20 left-20 overflow-hidden rounded-4xl">
+			<iframe title="preview" class="h-full w-full" src="http://localhost:{data.previewPort}"
+			></iframe>
+		</div>
+	</div>
+{/if}
+{#if data.previewPort}
+	<div class="fixed right-4 bottom-4 z-50">
+		<button
+			onclick={() => void (showPreview = !showPreview)}
+			title="Open/close Preview"
+			class="flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border bg-white hover:bg-gray-300 active:bg-gray-600"
+		>
+			<i class="fa-regular fa-eye"></i>
+		</button>
+	</div>
+{/if}
 
 <div class="flex w-full justify-center pt-4">
 	<div class="container">
